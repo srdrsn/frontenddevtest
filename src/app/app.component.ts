1eee75274cbce764889c570698f6192b66fd6134
srdrsn <dnesra55@gmail.com>
2022-05-30 23:02:15 +0200

    9 files modified, 8 added, 4 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component, VERSION } from '@angular/core';
+import { timer } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { FormComponent } from './form/form.component';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'Candidate';
+  constructor(private matDialog: MatDialog) {}
+
+  openDialog(): void {
+    const dialogRef = this.matDialog.open(FormComponent, {
+      width: '250px',
+      height: '300px',
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MaterialModule } from './shared/material-module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [AppComponent, FormComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatDialogModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: [FormComponent],
 })
-export class AppModule { }
+export class AppModule {}
